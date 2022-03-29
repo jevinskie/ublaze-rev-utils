065830b5a254c9aee45e25e59384c5709905a357
Jevin Sweval <jevinsweval@gmail.com>
2022-03-28 19:28:59 -0700

    wip

--- bflt2elf.cpp
+++ bflt2elf.cpp
@@ -23,9 +23,10 @@ template <typename T> T bswap(T v) {
     return v;
 }
 
-template <typename T> void bswap(T *v) {
+template <typename T> T bswap(T *v) {
     if constexpr (std::endian::native == std::endian::little)
         *v = ixm::byteswap(*v);
+    return *v;
 }
 
 uint8_t *readfile(const char *filename, std::size_t *len = nullptr, bool rw = true,
@@ -66,29 +67,65 @@ void writefile(const char *filename, uint8_t *buf, std::size_t len) {
 int main(int argc, const char **argv) {
     assert(argc == 3);
 
+    uint32_t load_addr = 0x1000'0000;
+    // uint32_t load_addr = 0x0;
+
     size_t in_sz = 0;
     auto in_buf  = readfile(argv[1], &in_sz, true);
 
-    auto bflt_hdr = (bflt_header_t *)in_buf;
-    assert(!memcmp(bflt_hdr->magic, "bFLT", 4));
-    assert(bswap(bflt_hdr->rev) == 4);
-    bswap(&bflt_hdr->entry);
-    bswap(&bflt_hdr->data_start);
-    bswap(&bflt_hdr->data_end);
-    bswap(&bflt_hdr->bss_end);
-    bswap(&bflt_hdr->stack_size);
-    bswap(&bflt_hdr->reloc_start);
-    bswap(&bflt_hdr->reloc_count);
+    auto bflt_hdr = *(bflt_header_t *)in_buf;
+    assert(!memcmp(bflt_hdr.magic, "bFLT", 4));
+    assert(bswap(bflt_hdr.rev) == 4);
+    bswap(&bflt_hdr.entry);
+    bswap(&bflt_hdr.data_start);
+    bswap(&bflt_hdr.data_end);
+    bswap(&bflt_hdr.bss_end);
+    bswap(&bflt_hdr.stack_size);
+    bswap(&bflt_hdr.reloc_start);
+    bswap(&bflt_hdr.reloc_count);
 
     auto code_buf  = (const char *)in_buf;
-    size_t code_sz = bflt_hdr->data_start;
+    size_t code_sz = bflt_hdr.data_start;
 
-    auto data_buf  = (const char *)in_buf + bflt_hdr->data_start;
-    size_t data_sz = bflt_hdr->data_end - bflt_hdr->data_start;
+    auto data_buf  = (const char *)in_buf + bflt_hdr.data_start;
+    size_t data_sz = bflt_hdr.data_end - bflt_hdr.data_start;
 
-    auto bss_sz = bflt_hdr->bss_end - bflt_hdr->data_end;
+    auto bss_sz = bflt_hdr.bss_end - bflt_hdr.data_end;
 
-    auto reloc_buf = (uint32_t *)(in_buf + bflt_hdr->reloc_start);
+    auto reloc_buf = (uint32_t *)(in_buf + bflt_hdr.reloc_start);
+
+    for (uint32_t i = 0; i < bflt_hdr.reloc_count; ++i) {
+        uint32_t addr = reloc_buf[i];
+        bswap(&addr);
+        uint32_t addr_val, inst_hi, inst_lo;
+        uint32_t *addr_p;
+        bool is64     = false;
+        if (addr & 0x8000'0000) {
+            addr &= ~0x8000'0000;
+            is64 = true;
+        }
+        if (addr < code_sz) {
+            addr_p = (uint32_t *)(in_buf + addr + sizeof(bflt_hdr));
+        } else {
+            printf("data reloc!\n");
+            addr_p = (uint32_t *)(in_buf + addr + sizeof(bflt_hdr));
+        }
+        printf("reloc[%04u]: 0x%08x %s addr_p: %p\n", i, addr, is64 ? "*" : "", addr_p);
+        if (!is64) {
+            addr_val = addr_p[0];
+            bswap(&addr_val);
+        } else {
+            inst_hi = addr_p[0];
+            bswap(&inst_hi);
+            inst_lo = addr_p[1];
+            bswap(&inst_lo);
+            addr_val = ((inst_hi & 0xFFFF) << 16) | (inst_lo & 0xFFFF);
+        }
+        uint32_t new_addr_val = addr_val + load_addr;
+        printf("addr_p: %p addr_val: 0x%08x new_addr_val: 0x%08x\n", addr_p, addr_val, new_addr_val);
+        // printf("new_addr_val: %p\n", new_addr_val);
+        // *addr_p = bswap(new_addr_val);
+    }
 
     elfio writer;
     writer.create(ELFCLASS32, ELFDATA2MSB);
@@ -96,11 +133,9 @@ int main(int argc, const char **argv) {
     writer.set_os_abi(ELFOSABI_LINUX);
     writer.set_type(ET_EXEC);
     writer.set_machine(EM_MICROBLAZE);
-    writer.set_entry(bflt_hdr->entry);
-
-    // uint32_t load_addr = 0x1000'0000;
-    uint32_t load_addr = 0x0;
-    writer.set_entry(load_addr);
+    uint32_t elf_entry = bflt_hdr.entry + load_addr;
+    printf("elf_entry: 0x%08x\n", elf_entry);
+    writer.set_entry(elf_entry);
     auto load_seg = writer.segments.add();
     load_seg->set_type(PT_LOAD);
     load_seg->set_virtual_address(load_addr);
@@ -135,19 +170,6 @@ int main(int argc, const char **argv) {
     // // text_rel->set_link( sym_sec->get_index() );
     // text_rel->set_addr_align(4);
     // text_rel->set_entry_size(writer.get_default_entry_size(SHT_REL));
-
-    // relocation_section_accessor rel_writer(writer, text_rel);
-    for (uint32_t i = 0; i < bflt_hdr->reloc_count; ++i) {
-        bswap(&reloc_buf[i]);
-        uint32_t addr = reloc_buf[i];
-        bool is64     = false;
-        if (addr & 0x8000'0000) {
-            addr &= ~0x8000'0000;
-            is64 = true;
-        }
-        // rel_writer.add_entry(addr, !is64 ? R_MICROBLAZE_32 : R_MICROBLAZE_64);
-        // printf("reloc[%04u]: 0x%08x%s\n", i, addr, is64 ? " *" : "");
-    }
 
     writer.save(argv[2]);
     assert(!chmod(argv[2], 0755));
